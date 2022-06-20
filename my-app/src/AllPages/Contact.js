44fcc31d7cf29c22e3fc4542fa0bb8ab6890b372
Priyanka <priyankasj120@gmail.com>
2022-06-20 15:10:56 +0530

    Added

---
+++ my-app/src/AllPages/Contact.js
@@ -0,0 +1,12 @@
+const Contact=()=>{
+    return <>
+    <div>
+        <h2>Contact</h2>
+        Name: <input type="text" placeholder="enter your name"/>
+        Address: <input type="text" placeholder="address"/>
+        Phone Number: <input type="text" placeholder="phone number"/>
+        <button type="submit">Submit</button>
+    </div>
+    </>
+}
+export default Contact;
